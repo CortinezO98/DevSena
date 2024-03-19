d1298ba86080707aa4d426b4c2f8ed8fedcff316
CortinezO98 <jcortinezosorio@gmail.com>
2024-03-19 11:22:15 -0500

    Ajustes servidor

--- sena/sena/settings.py
+++ sena/sena/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = "sena.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sena',
+        'USER': 'admin',
+        'PASSWORD': '1234a',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        #"ENGINE": "django.db.backends.sqlite3",
+        #"NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
