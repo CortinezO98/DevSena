e668b68cf131b6cee9a1ac5986e3b08335dc3123
CortinezO98 <jcortinezosorio@gmail.com>
2024-03-20 13:12:01 -0500

    Implementación de django-import-export

--- sena/principal/admin.py
+++ sena/principal/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import *
 
-admin.site.register(Accion)
-admin.site.register(RegistroAccion)
-admin.site.register(Encuesta)
-admin.site.register(RegistroDatosUser)
+class AccionResource(resources.ModelResource):
+    class Meta:
+        model = Accion
+        
+class AccionAdmin(ImportExportModelAdmin):
+    resource_classes = [AccionResource]
+    list_display = ('id','nombre',)
+    search_fields = ['id','nombre']
+
+class RegistroAccionResource(resources.ModelResource):
+    class Meta:
+        model = RegistroAccion
+        
+class RegistroAccionAdmin(ImportExportModelAdmin):
+    resource_classes = [RegistroAccionResource]
+    
+class EncuestaResource(resources.ModelResource):
+    class Meta:
+        model = Encuesta
+        
+class EncuestaAdmin(ImportExportModelAdmin):
+    resource_classes = [EncuestaResource]
+    list_display = ('dominioPersonaAtendio','satisfaccionServicioRecibido', 'recomendacionCanalAtencion', 'solucionSolicitud','ip','usuario',)
+    
+class RegistroDatosUserResource(resources.ModelResource):
+    class Meta:
+        model = RegistroDatosUser
+        
+class RegistroDatosUserAdmin(ImportExportModelAdmin):
+    resource_classes = [RegistroDatosUserResource]
+    list_display = ('tipo_contacto','tipo_documento','numero_documento','nombres','apellidos','sede_contacto','ip_dispositivo',)
+    search_fields = ['numero_documento','nombres','apellidos','sede_contacto','ip_dispositivo']
+
+admin.site.register(Accion, AccionAdmin)
+admin.site.register(RegistroAccion, RegistroAccionAdmin)
+admin.site.register(Encuesta, EncuestaAdmin)
+admin.site.register(RegistroDatosUser, RegistroDatosUserAdmin)
