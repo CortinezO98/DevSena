167a44f7721e1ca1da3f9f7b2bc0ff4ddca15867
CortinezO98 <jcortinezosorio@gmail.com>
2024-07-24 12:29:25 -0500

    list filter en registro de datos, se podra filtrar por ip, por sede contacto y numero de documento y se quito acciones en el panel de admin

--- sena/principal/admin.py
+++ sena/principal/admin.py
@@ -3,15 +3,6 @@ from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from .models import *
 
-class AccionResource(resources.ModelResource):
-    class Meta:
-        model = Accion
-        
-class AccionAdmin(ImportExportModelAdmin):
-    resource_classes = [AccionResource]
-    list_display = ('id','nombre',)
-    search_fields = ['id','nombre']
-
 class RegistroAccionResource(resources.ModelResource):
     class Meta:
         model = RegistroAccion
@@ -19,7 +10,7 @@ class RegistroAccionResource(resources.ModelResource):
 class RegistroAccionAdmin(ImportExportModelAdmin):
     resource_classes = [RegistroAccionResource]
     list_display = ('accion','ip','fecha')
-    list_filter = ['fecha'] 
+    list_filter = ['fecha','ip'] 
         
 class RegistroDatosUserResource(resources.ModelResource):
     class Meta:
@@ -29,7 +20,8 @@ class RegistroDatosUserAdmin(ImportExportModelAdmin):
     resource_classes = [RegistroDatosUserResource]
     list_display = ('tipo_contacto','tipo_documento','numero_documento','nombres','apellidos','sede_contacto','ip_dispositivo',)
     search_fields = ['numero_documento','nombres','apellidos','sede_contacto','ip_dispositivo']
-    
+    list_filter = ['numero_documento','sede_contacto','ip_dispositivo']
+
 admin.site.register(Accion, AccionAdmin)
 admin.site.register(RegistroAccion, RegistroAccionAdmin)
 admin.site.register(RegistroDatosUser, RegistroDatosUserAdmin)
